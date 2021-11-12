02348fcf76aeafcb1f92497a2431706711152c97
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2021-11-12 15:32:03 +0100

    created the Indexer

--- src/main/java/ListOfFiles.kt
+++ src/main/java/ListOfFiles.kt
@@ -1,16 +1,18 @@
 import java.io.File
 import java.io.FilenameFilter
+import java.io.IOException
 
-class ListOfFiles (private val documentsPath: String = "./documents/"){
+class ListOfFiles(private val documentsPath: String = "./documents/") {
+    @Throws(IOException::class)
     fun getDocs(): Array<out File>? {
         //Creating a File object for directory
         val directoryPath = File(documentsPath)
-        val textFileFilter = FilenameFilter{ _, name ->
+        val textFileFilter = FilenameFilter { _, name ->
             val lowercaseName = name.lowercase()
             lowercaseName.endsWith(".txt") ||
                     lowercaseName.endsWith(".pdf") ||
-                        lowercaseName.endsWith(".doc") ||
-                            lowercaseName.endsWith(".docx")
+                    lowercaseName.endsWith(".doc") ||
+                    lowercaseName.endsWith(".docx")
         }
         return directoryPath.listFiles(textFileFilter)
     }
